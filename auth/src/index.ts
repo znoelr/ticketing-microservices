629662c4dd32906db074075fdc587a4b8ec226df
znoelr <rennleonz@gmail.com>
2024-03-15 18:04:57 -0500

    Setup cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 import authRouter from './auth.router';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { connectDb } from './db';
@@ -8,12 +9,19 @@ connectDb();
 
 const app = express();
 
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}));
+
 app.use((req, res, next) => {
   console.log('Got request', req.url);
   next();
 });
 
-app.use(express.json());
 app.use('/auth', authRouter);
 app.use(errorHandler);
 
