3119cee0644a70b4c0e2ed51ce54838de9166164
znoelr <rennleonz@gmail.com>
2024-03-15 18:36:35 -0500

    Setup jwt creation

--- auth/src/auth.controller.ts
+++ auth/src/auth.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
+import jwt from 'jsonwebtoken';
 import { BadRequestError, RequestValidationError } from "./errors";
 import { User } from "./db/models/user.model";
 
@@ -15,5 +16,11 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
   }
   const newUser = await User.build({ email, password });
   await newUser.save();
+  // Generate JWT
+  const token = jwt.sign({
+    id: newUser._id,
+    email: newUser.email,
+  }, 'secret_key');
+  req.session = { jwt: token };
   res.json(newUser);
 }
