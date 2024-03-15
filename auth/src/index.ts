d59c10633d19aad322f71f6640dfc02f5dee70c7
znoelr <rennleonz@gmail.com>
2024-03-14 21:06:32 -0500

    Setup express asyn erros

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import authRouter from './auth.router';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { connectDb } from './db';
