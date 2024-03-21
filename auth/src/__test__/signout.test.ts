f47724e7e8f1c6af5cead5a4d6b9a783b1abcc28
znoelr <rennleonz@gmail.com>
2024-03-20 19:26:49 -0500

    Implement signout tests

---
+++ auth/src/__test__/signout.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../app';
+
+it('clears the cookie after signout', async () => {
+  await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'test@gmail.com',
+      password: 'abcde$12345',
+    })
+    .expect(201);
+
+  const response = await request(app)
+  .get('/auth/signout')
+  .expect(200);
+
+  expect(response.get('Set-Cookie')[0])
+    .toBe('session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly');
+});
