a8cba2f4cdab322176a0d22acbe6f3b9b7f8aee9
znoelr <rennleonz@gmail.com>
2024-03-20 19:49:00 -0500

    Add new tests for get current user

--- auth/src/__test__/get-me.test.ts
+++ auth/src/__test__/get-me.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import { app } from '../app';
 
 it('gets the logged in user', async () => {
-  const signupResponse = await request(app)
-    .post('/auth/signup')
-    .send({
-      email: 'test@gmail.com',
-      password: 'abcde$12345',
-    })
-    .expect(201);
-  const cookie = signupResponse.get('Set-Cookie');
+  const cookie = await global.singup(app);
 
   const response = await request(app)
   .get('/auth/me')
@@ -18,4 +11,13 @@ it('gets the logged in user', async () => {
   .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@gmail.com');
+});
+
+it('responsds with null if not authenticated', async () => {
+  const response = await request(app)
+  .get('/auth/me')
+  .send()
+  .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
