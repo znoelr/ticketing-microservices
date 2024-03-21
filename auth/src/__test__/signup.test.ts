55f7d09fac85a44ac8f0627e9240201d715aaf74
znoelr <rennleonz@gmail.com>
2024-03-20 19:14:02 -0500

    Implement signup test

--- auth/src/__test__/signup.test.ts
+++ auth/src/__test__/signup.test.ts
@@ -1,12 +1,78 @@
 import request from 'supertest';
 import { app } from '../app';
 
-it('returns a 201 on successful signup', () => {
-  request(app)
+it('returns a 201 on successful signup', async () => {
+  await request(app)
     .post('/auth/signup')
     .send({
       email: 'test@gmail.com',
       password: 'abcde$12345',
     })
     .expect(201);
+});
+
+it('returns a 400 on invalid email', async () => {
+  await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'kajsndwq',
+      password: 'abcde$12345',
+    })
+    .expect(400);
+});
+
+it('returns a 400 on invalid password', async () => {
+  await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'email@gmail.com',
+      password: '1w',
+    })
+    .expect(400);
+});
+
+it('returns a 400 on missing email and password', async () => {
+  await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'some@gmail.com'
+    })
+    .expect(400);
+
+    await request(app)
+    .post('/auth/signup')
+    .send({
+      password: 'aksjdn2k343242k34jn'
+    })
+    .expect(400);
+});
+
+it('returns a 400 on duplicate email', async () => {
+  await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'some@gmail.com',
+      password: 'aksjdn2k343242k34jn',
+    })
+    .expect(201);
+
+    await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'some@gmail.com',
+      password: 'aksjdn2k343242k34jn',
+    })
+    .expect(400);
+});
+
+it('sets a cookie on successful signup', async () => {
+  const response = await request(app)
+    .post('/auth/signup')
+    .send({
+      email: 'some@gmail.com',
+      password: 'aksjdn2k343242k34jn',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
 });
--- auth/src/app.ts
+++ auth/src/app.ts
@@ -11,14 +11,8 @@ app.set('trust proxy', true);
 app.use(express.json());
 app.use(cookieSession({
   signed: false,
-  secure: true,
+  secure: process.env.NODE_ENV !== 'test',
 }));
-
-app.use((req, res, next) => {
-  console.log('Got request', req.url);
-  next();
-});
-
 app.use('/auth', authRouter);
 app.use(errorHandler);
 
