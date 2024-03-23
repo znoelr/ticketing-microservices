f76a41896db0953e1ccf4c8b07245f2a0b620e79
znoelr <rennleonz@gmail.com>
2024-03-23 05:30:14 -0500

    [tickets] Setup tickets router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@mss-ticketing/common';
+import { currentUser, errorHandler, NotFoundError } from '@mss-ticketing/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+
+app.use('/api/tickets', createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError('[404] Not Found');
