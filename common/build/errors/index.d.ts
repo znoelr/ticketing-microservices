26cf68a1df3b74966c895ebf591b27e0dfe19ce7
znoelr <rennleonz@gmail.com>
2024-03-23 20:34:45 -0500

    [common] Create '.gitignore' and remove cached git 'build' folder

--- common/build/errors/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './custom-error';
-export * from './request-validation.error';
-export * from './bad-request-.error';
-export * from './unauthorized.error';
-export * from './not-found.error';
--- common/build/middlewares/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './current-user.middleware';
-export * from './error-handler.middleware';
-export * from './require-auth.middleware';
-export * from './validate-request.middleware';
