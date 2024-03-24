c7ee2b7e6e09f927218d3e86acefeb59064ad100
znoelr <rennleonz@gmail.com>
2024-03-23 20:39:42 -0500

    [tickets] Integrate downloaded code

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { currentUser, errorHandler, NotFoundError } from '@mss-ticketing/common';
+import { errorHandler, NotFoundError, currentUser } from '@mss-ticketing/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,13 +17,15 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
 app.use(currentUser);
 
-app.use('/api/tickets', createTicketRouter);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError('[404] Not Found');
+  throw new NotFoundError('Not found');
 });
 
 app.use(errorHandler);
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,5 +1,7 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
 import jwt from 'jsonwebtoken';
 
 declare global {
@@ -35,17 +37,22 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload.  { id, email }
   const payload = {
-    id: '1lk24j124l',
-    email: 'test@test.com'
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
   };
+
   // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token };
+
   // Turn that session into JSON
   const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
   const base64 = Buffer.from(sessionJSON).toString('base64');
+
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
 };
