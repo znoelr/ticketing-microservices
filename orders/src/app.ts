344f5d3cfc92d65d36ed043c9b5eeea32ee3b3db
znoelr <rennleonz@gmail.com>
2024-03-27 00:49:58 -0500

    [orders] Integrate code

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@mss-ticketing/common';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes/index';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +18,11 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,32 @@
+import { NotFoundError, OrderStatus, UnauthorizedError, requireAuth } from '@mss-ticketing/common';
+import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
+
+    res.status(204).send(order);
+  }
+);
+
+
+export { router as deleteOrderRouter };
