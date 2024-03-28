83f6d3b34b60aabe0ed9d5325a75d881e34c2ce8
znoelr <rennleonz@gmail.com>
2024-03-28 06:38:46 -0500

    [tickets] Integrate 'order-created' listener

--- tickets/src/events/listeners/order-created.listener.ts
+++ tickets/src/events/listeners/order-created.listener.ts
@@ -1,12 +1,24 @@
 import { BaseNatsListener, OrderCreatedEvent, OrderCreatedPayload, OrderSubjects } from "@mss-ticketing/common";
 import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedNatsListener extends BaseNatsListener<OrderCreatedEvent> {
   readonly subject = OrderSubjects.Created;
-  queueGroupName: string = 'payments-service';
+  queueGroupName: string = queueGroupName;
 
-  onMessage(data: OrderCreatedPayload, msg: Message): void {
-    console.log(data);
+  async onMessage(data: OrderCreatedPayload, msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+    // Save the ticket
+    await ticket.save();
+    // ack the message
     msg.ack();
   }
 }
