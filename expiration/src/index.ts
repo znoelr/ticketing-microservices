74e8b9fdb47c814a86aabba3e51668e1e8ea8498
znoelr <rennleonz@gmail.com>
2024-03-28 23:26:16 -0500

    [expiration] Integrate 'expiration' app

---
+++ expiration/src/index.ts
@@ -0,0 +1,31 @@
+import { NatsClient } from "@mss-ticketing/common";
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+    await NatsClient.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    NatsClient.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => NatsClient.client.close());
+    process.on('SIGTERM', () => NatsClient.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
