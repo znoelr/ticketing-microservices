fe676b115f9e2d254867c41a87ddeaa05294f590
znoelr <rennleonz@gmail.com>
2024-03-29 00:35:53 -0500

    [expiration] Implement expiration job queue

--- expiration/src/events/listeners/order-created.listener.ts
+++ expiration/src/events/listeners/order-created.listener.ts
@@ -1,12 +1,20 @@
 import { BaseNatsListener, OrderCreatedEvent, OrderCreatedPayload, OrderSubjects } from "@mss-ticketing/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedNatsListener extends BaseNatsListener<OrderCreatedEvent> {
   readonly subject = OrderSubjects.Created;
   queueGroupName: string = queueGroupName;
 
   async onMessage(data: OrderCreatedPayload, msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Delay', delay);
+    await expirationQueue.add({
+      orderId: data.id,
+    }, {
+      delay,
+    });
     // ack the message
     msg.ack();
   }
