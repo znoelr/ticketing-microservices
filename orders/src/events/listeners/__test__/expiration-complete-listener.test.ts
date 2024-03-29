c6ac1fe9623dbb67e3a01f9a4b989f3ced6f1a38
znoelr <rennleonz@gmail.com>
2024-03-29 01:26:51 -0500

    [orders] Integrate 'expiration-complete' listener tests

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../../models/order';
+import { Ticket } from '../../../models/ticket';
+import { ExpirationCompleteNatsListener } from '../expiration-complete.listener';
+import { ExpirationCompleteEvent, NatsClient, OrderStatus } from '@mss-ticketing/common';
+
+const setup = async () => {
+  const listener = new ExpirationCompleteNatsListener(NatsClient.client);
+
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: 'alskdfj',
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
+
+it('updates the order status to cancelled', async () => {
+  const { listener, order, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emit an OrderCancelled event', async () => {
+  const { listener, order, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(NatsClient.client.publish).toHaveBeenCalled();
+
+  const eventData = JSON.parse(
+    (NatsClient.client.publish as jest.Mock).mock.calls[0][1]
+  );
+  expect(eventData.id).toEqual(order.id);
+});
+
+it('ack the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
