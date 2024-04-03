9687a08b85a127a254b02c1d7cb9348a12cd7c60
znoelr <rennleonz@gmail.com>
2024-04-03 14:25:05 -0500

    [payments] Setup listeners on startup

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { NatsClient } from '@mss-ticketing/common';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -31,6 +33,9 @@ const start = async () => {
     });
     process.on('SIGINT', () => NatsClient.client.close());
     process.on('SIGTERM', () => NatsClient.client.close());
+
+    new OrderCreatedListener(NatsClient.client).listen();
+    new OrderCancelledListener(NatsClient.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
