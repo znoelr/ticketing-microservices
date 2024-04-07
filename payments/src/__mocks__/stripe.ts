9cb3935153d1da0983f06d93430d9b2f4e6e1b8e
znoelr <rennleonz@gmail.com>
2024-04-06 22:42:13 -0500

    [payments] Update 'stripe' mock

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -1,5 +1,15 @@
+import { randomBytes } from 'crypto';
+
+const _chargesList: any[] = [];
+
 export const stripe = {
   charges: {
-    create: jest.fn().mockResolvedValue({}),
+    create: jest.fn().mockImplementation(({ currency, amount, source }) => {
+      const id = randomBytes(16).toString('hex');
+      const newCharge = { id, currency, amount, source };
+      _chargesList.push(newCharge);
+      return newCharge;
+    }),
+    list: jest.fn().mockResolvedValue({ data: _chargesList }),
   },
 };
