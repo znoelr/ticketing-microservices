26b9cc8d984dd6a81149593de0ba951d8b39015e
znoelr <rennleonz@gmail.com>
2024-04-06 22:38:44 -0500

    [payments] Update 'new' route to save 'Payment' after creating a charge

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@mss-ticketing/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -33,11 +34,16 @@ router.post(
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: Math.floor(order.price * 100),
       source: token,
     });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
